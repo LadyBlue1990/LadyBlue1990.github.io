fa640f83362ac27887ea0447545020547a53fac4
Alexandra Zajaczkowski <alexandrajaninez@gmail.com>
2023-06-05 14:57:37 -0400

    Added a Home page to the header

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,11 +1,19 @@
 function Menu() {
 
 return(
-    <div className="grid w-full place-items-center overflow-x-scroll rounded-lg p-6 lg:overflow-visible bg-gradient-to-b from-indigo-300 to- bg-orange-100">        
+    <div className="grid w-full place-items-center overflow-x-scroll p-10 lg:overflow-visible bg-gradient-to-b from-indigo-300 to- bg-orange-100">        
         <div className="w-full">
           <div className="relative right-0">
        
             <ul className="relative flex list-none flex-wrap">
+             <li className="z-30 flex-auto text-center">
+                <a
+                  className="text-slate-700 z-30 mb-0 flex w-full cursor-pointer items-center justify-center rounded-lg border-0 bg-inherit px-0 py-1 transition-all ease-in-out"
+                  href="index.html"
+                >
+                  <button className="fun-button">Home</button>
+                  </a>
+                  </li>
               <li className="z-30 flex-auto text-center">
                 <a
                   className="text-slate-700 z-30 mb-0 flex w-full cursor-pointer items-center justify-center rounded-lg border-0 bg-inherit px-0 py-1 transition-all ease-in-out"
